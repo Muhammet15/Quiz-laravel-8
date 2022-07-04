b6a717dc7297261db5a3873d2d011b8460fa4614
kayacan <kayacan@null.com>
2022-07-04 16:29:21 +0300

    Boostrap ve jetstream kurulumları yapıldı, profil update düzenlendi, tema düzenlendi..

--- database/migrations/2022_07_04_112643_create_sessions_table.php
+++ database/migrations/2022_07_04_112643_create_sessions_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
             $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->integer('last_activity')->index();
         });
     }
 
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('sessions');
     }
 };
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+    <x-slot name="header">Anasayfa
+        
+    </x-slot>
+    <div class="alert alert-primary" role="alert"> This is message .. </div>
+
+</x-app-layout>
